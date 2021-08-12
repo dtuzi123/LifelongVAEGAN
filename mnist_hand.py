98c694abc8e3f0fe849145feaf122f6158c659df
Fei Ye <fy689@york.ac.uk>
2021-08-12 16:18:30 +0100

    Add files via upload

---
+++ mnist_hand.py
@@ -0,0 +1,183 @@
+import random
+import numpy as np
+from Fei_dataset import *
+from six.moves import xrange
+from scipy.misc import imsave as ims
+from HSICSupport import *
+from ops import *
+from Utlis2 import *
+import gzip
+import cv2
+import keras as keras
+
+def load_mnist(dataset_name):
+    data_dir = os.path.join("./data", dataset_name)
+
+    def extract_data(filename, num_data, head_size, data_size):
+        with gzip.open(filename) as bytestream:
+            bytestream.read(head_size)
+            buf = bytestream.read(data_size * num_data)
+            data = np.frombuffer(buf, dtype=np.uint8).astype(np.float)
+        return data
+
+    data = extract_data(data_dir + '/train-images-idx3-ubyte.gz', 60000, 16, 28 * 28)
+    trX = data.reshape((60000, 28, 28, 1))
+
+    data = extract_data(data_dir + '/train-labels-idx1-ubyte.gz', 60000, 8, 1)
+    trY = data.reshape((60000))
+
+    data = extract_data(data_dir + '/t10k-images-idx3-ubyte.gz', 10000, 16, 28 * 28)
+    teX = data.reshape((10000, 28, 28, 1))
+
+    data = extract_data(data_dir + '/t10k-labels-idx1-ubyte.gz', 10000, 8, 1)
+    teY = data.reshape((10000))
+
+    trY = np.asarray(trY)
+    teY = np.asarray(teY)
+
+    X = np.concatenate((trX, teX), axis=0)
+    y = np.concatenate((trY, teY), axis=0).astype(np.int)
+
+    seed = 547
+    np.random.seed(seed)
+    np.random.shuffle(X)
+    np.random.seed(seed)
+    np.random.shuffle(y)
+
+    y_vec = np.zeros((len(y), 10), dtype=np.float)
+    for i, label in enumerate(y):
+        y_vec[i, y[i]] = 1.0
+
+    return X / 255., y_vec
+
+def Give_InverseFashion32():
+    mnistName = "Fashion"
+    data_X, data_y = load_mnist(mnistName)
+    data_X = np.reshape(data_X, (-1, 28, 28))
+    for i in range(np.shape(data_X)[0]):
+        for k1 in range(28):
+            for k2 in range(28):
+                data_X[i, k1, k2] = 1.0 - data_X[i, k1, k2]
+
+    data_X = np.reshape(data_X, (-1, 28, 28, 1))
+    data_X = np.concatenate((data_X, data_X, data_X), axis=-1)
+    size = (int(32), int(32))
+    myArr = []
+    for i in range(np.shape(data_X)[0]):
+        image = cv2.resize(data_X[i], size, interpolation=cv2.INTER_AREA)
+        myArr.append(image)
+
+    data_X = np.array(myArr)
+
+    x_train = data_X[0:60000]
+    x_test = data_X[60000:70000]
+    y_train = data_y[0:60000]
+    y_test = data_y[60000:70000]
+
+    mnist_train_x = x_train
+    mnist_train_label = y_train
+    mnist_test = x_test
+    mnist_label_test = y_test
+
+    return mnist_train_x,mnist_train_label,mnist_test,mnist_label_test
+
+def GiveMNIST_SVHN():
+    mnistName = "mnist"
+    data_X, data_y = load_mnist(mnistName)
+
+    #data_X = np.expand_dims(data_X, axis=3)
+    data_X = np.concatenate((data_X, data_X, data_X), axis=-1)
+
+    size = (int(32), int(32))
+    myArr = []
+    for i in range(np.shape(data_X)[0]):
+        image = cv2.resize(data_X[i],size, interpolation=cv2.INTER_AREA)
+        myArr.append(image)
+
+    data_X = np.array(myArr)
+
+    x_train = data_X[0:60000]
+    x_test = data_X[60000:70000]
+    y_train = data_y[0:60000]
+    y_test = data_y[60000:70000]
+
+    mnist_train_x = x_train
+    mnist_train_label = y_train
+    mnist_test = x_test
+    mnist_label_test = y_test
+
+
+    myTest = mnist_train_x[0:64]
+
+    ims("results/" + "gggg" + str(0) + ".jpg", merge2(myTest[:64], [8, 8]))
+
+    x_train, y_train, x_test, y_test = GetSVHN_DataSet()
+    y_train = keras.utils.to_categorical(y_train)
+    y_test = keras.utils.to_categorical(y_test)
+
+    return mnist_train_x,mnist_train_label,mnist_test,mnist_label_test,x_train,y_train,x_test,y_test
+
+def GiveFashion32():
+    mnistName = "Fashion"
+    data_X, data_y = load_mnist(mnistName)
+
+    # data_X = np.expand_dims(data_X, axis=3)
+    data_X = np.concatenate((data_X, data_X, data_X), axis=-1)
+
+    size = (int(32), int(32))
+    myArr = []
+    for i in range(np.shape(data_X)[0]):
+        image = cv2.resize(data_X[i], size, interpolation=cv2.INTER_AREA)
+        myArr.append(image)
+
+    data_X = np.array(myArr)
+
+    x_train = data_X[0:60000]
+    x_test = data_X[60000:70000]
+    y_train = data_y[0:60000]
+    y_test = data_y[60000:70000]
+
+    mnist_train_x = x_train
+    mnist_train_label = y_train
+    mnist_test = x_test
+    mnist_label_test = y_test
+
+    return mnist_train_x,mnist_train_label,mnist_test,mnist_label_test
+
+def Split_dataset(x,y,n_label):
+    y = np.argmax(y,axis=1)
+    n_each = n_label / 10
+    isRun = True
+    x_train = []
+    y_train = []
+    index = np.zeros(10)
+    while(isRun):
+        a = random.randint(0, np.shape(x)[0])-1
+        x1 = x[a]
+        y1 = y[a]
+        if index[y1] < n_each:
+            x_train.append(x1)
+            y_train.append(y1)
+            index[y1] = index[y1]+1
+        isOk1 = True
+        for i in range(10):
+            if index[i] < n_each:
+                isOk1 = False
+        if isOk1:
+            break
+
+    x_train = np.array(x_train)
+    y_train = np.array(y_train)
+    return x_train,y_train
+
+def Give_InverseFashion():
+    mnistName = "Fashion"
+    data_X, data_y = load_mnist(mnistName)
+    data_X = np.reshape(data_X,(-1,28,28))
+    for i in range(np.shape(data_X)[0]):
+        for k1 in range(28):
+            for k2 in range(28):
+                data_X[i,k1,k2] = 1.0 - data_X[i,k1,k2]
+
+    data_X = np.reshape(data_X,(-1,28,28,1))
+    return data_X,data_y
---
+++ utils.py
@@ -0,0 +1,113 @@
+import numpy as np
+import tensorflow as tf
+
+def generate_numvec(digit, z = None):
+    # +2 は潜在空間の次元分
+    out = np.zeros((1, 12))
+    out[:, digit + 2] = 1.
+    if z is None:
+        return out
+    else:
+        # サンプリング空間が指定されているときに，その情報を落とし込んでおく
+        for i in range(len(z)):
+            out[:,i] = z[i]
+        return out
+
+def merge3(images,size):
+    h, w = images.shape[1], images.shape[2]
+    img = np.zeros((h, w * size,3))
+
+    for idx, image in enumerate(images):
+        j = int(idx)
+        img[0:h, int(j*h):int(j*h+h),0:3] = image
+
+    return img
+
+def merge2(images,size):
+    h, w = images.shape[1], images.shape[2]
+    img = np.zeros((h * size[0], w * size[1],3))
+
+    for idx, image in enumerate(images):
+        i = idx % size[1]
+        j = idx / size[1]
+        j = int(j)
+
+        img[int(j*h):int(j*h+h), int(i*w):int(i*w+w),0:3] = image
+
+    return img
+
+def merge(images, size):
+    h, w = images.shape[1], images.shape[2]
+    img = np.zeros((h * size[0], w * size[1]))
+
+    for idx, image in enumerate(images):
+        i = idx % size[1]
+        j = idx / size[1]
+        j = int(j)
+
+        img[int(j*h):int(j*h+h), int(i*w):int(i*w+w)] = image
+
+    return img
+
+from random import shuffle
+import scipy.misc
+import numpy as np
+
+def center_crop(x, crop_h, crop_w=None, resize_w=64):
+    # crop the images to [crop_h,crop_w,3] then resize to [resize_h,resize_w,3]
+    if crop_w is None:
+        crop_w = crop_h # the width and height after cropped
+    h, w = x.shape[:2]
+    j = int(round((h - crop_h)/2.))
+    i = int(round((w - crop_w)/2.))
+    return scipy.misc.imresize(x[j:j+crop_h, i:i+crop_w],
+                               [resize_w, resize_w])
+
+def merge55(images, size):
+    # merge all output images(of sample size:8*8 output images of size 64*64) into one big image
+    h, w = images.shape[1], images.shape[2]
+    img = np.zeros((h * size[0], w * size[1], 3))
+    for idx, image in enumerate(images): # idx=0,1,2,...,63
+        i = idx % size[1] # column number
+        j = idx // size[1] # row number
+        img[j*h:j*h+h, i*w:i*w+w, :] = image
+    return img
+
+def transform(image, npx=64, is_crop=True, resize_w=64):
+    if is_crop:
+        cropped_image = center_crop(image, npx, resize_w=resize_w)
+    else:
+        cropped_image = image
+    return np.array(cropped_image)/127.5 - 1.  # change pixel value range from [0,255] to [-1,1] to feed into CNN
+
+def inverse_transform(images):
+    return (images+1.)/2. # change image pixel value(outputs from tanh in range [-1,1]) back to [0,1]
+
+def imread(path, is_grayscale = False):
+    if (is_grayscale):
+        return scipy.misc.imread(path, flatten = True).astype(np.float) # [width,height] flatten RGB image to grayscale image
+    else:
+        return scipy.misc.imread(path).astype(np.float) # [width,height,color_dim]
+
+def imsave(images, size, path):
+    return scipy.misc.imsave(path, merge(images, size))
+
+def get_image2(image_path, image_size, is_crop=True, resize_w=64, is_grayscale = False):
+    return transform(imread(image_path, is_grayscale), image_size, is_crop, resize_w)
+
+def save_images(images, size, image_path):
+    # size indicates how to arrange the images to form a big summary image
+    # images: [batchsize,height,width,color]
+    # example: save_images(img, [8, 8],'./{}/train_{:02d}_{:04d}.png'.format(FLAGS.sample_dir, epoch, idx))
+    return imsave(inverse_transform(images), size, image_path)
+
+def save_images_256(images, size, image_path):
+    images = inverse_transform(images)
+    h, w = 64, 64 # 256,256
+    img = np.zeros((h * size[0], w * size[1], 3))
+    for idx, image in enumerate(images): # idx=0,1,2,...,63
+        image = scipy.misc.imresize(image,[h,w])
+        i = idx % size[1] # column number
+        j = idx // size[1] # row number
+        img[j*h:j*h+h, i*w:i*w+w, :] = image
+    return scipy.misc.imsave(image_path, img)
